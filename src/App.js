85969bf0ca4cc9fdf4f6e297ccec86936a00d669
Harcourt <harcourtbullv@gmail.com>
2022-06-12 22:42:41 -0400

    cant get input state to reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import LabeledTextInput from './components/LabeledTextInput';
+import Button from './components/Button';
 
 function App() {
     const data = useRef(document.getElementById('FIELD_555'));
     const [ state, setState ] = useState({});
+
+    const resetState = () => {
+        setState({});
+    };
 
     const handleUpdate = (key, value) => {
         setState(prevState => ({
@@ -17,12 +22,12 @@ function App() {
         data.current.value = JSON.stringify(state);
     }, [state]);
 
-
     return (
         <div>
             <LabeledTextInput label='Name' handleUpdate={handleUpdate} />
             <LabeledTextInput label='Title' handleUpdate={handleUpdate} />
             <LabeledTextInput label='Company' handleUpdate={handleUpdate} />
+            <Button text='Reset' callback={resetState} />
         </div>
     );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+const Button = ({ text, callback }) => {
+    return (
+        <button onClick={ callback }>{ text }</button>
+    )
+}
+
+export default Button;
